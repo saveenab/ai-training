7795698bba7b744bbac05664c4a2ccc0d9fcd654
Saveena <saveena.b@computingconceptsinc.com>
2026-06-03 16:34:24 -0400

    Add Docker, docker-compose, update.py

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 # Saveena Boga - Database Connection
 
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://saveenaboga@localhost:5432/task3_db"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://saveenaboga@localhost:5432/task3_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
